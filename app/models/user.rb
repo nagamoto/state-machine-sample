e449d50e8410590aa352080cbd1cd996ccb50014
ngmt <ngmt83ngmt@gmail.com>
2019-02-06 16:52:39 +0900

    Userにstatusを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,43 @@ class User < ApplicationRecord
 
   has_many :suspensions, class_name: 'UserSuspension'
   has_many :authentication_requests, class_name: 'UserAuthenticationRequest'
+
+
+  def status
+    return 'activated' if activated?
+    return 'suspended' if suspended?
+    return 'banned' if banned?
+    return 'closed' if closed?
+    return 'registered'
+  end
+
+  def activated?
+    return false unless detail.present?
+    return false if suspended? || banned? || closed?
+    true
+  end
+
+  def not_authenticated?
+    !authenticated?
+  end
+
+  def authentication_requested?
+    authentication_requests.exists?
+  end
+
+  def authenticated?
+    authentication_requests.where.not(authenticated_at: nil).exists?
+  end
+
+  def suspended?
+    suspensions.where(removed_at: nil).exists?
+  end
+
+  def banned?
+    ban.present?
+  end
+
+  def closed?
+    ban.present?
+  end
 end
