6309845e690af47c602b5deab3f11727f1160e33
ngmt <ngmt83ngmt@gmail.com>
2019-02-06 16:25:11 +0900

    Userに他モデルとの関連を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_one :detail, class_name: 'UserDetail'
+  has_one :close, class_name: 'UserClose'
+  has_one :ban, class_name: 'UserBan'
+
+  has_many :suspensions, class_name: 'UserSuspension'
+  has_many :authentication_requests, class_name: 'UserAuthenticationRequest'
 end
