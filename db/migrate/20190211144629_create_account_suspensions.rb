256223bbd7b3732bf8bd98e3dcd1de3cb92d5fe9
ngmt <ngmt83ngmt@gmail.com>
2019-02-11 23:46:43 +0900

    CMD: rails g model account_suspension removed_at:timestamp account:references

---
+++ db/migrate/20190211144629_create_account_suspensions.rb
@@ -0,0 +1,10 @@
+class CreateAccountSuspensions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :account_suspensions do |t|
+      t.timestamp :removed_at
+      t.references :account, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/account_suspension_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AccountSuspensionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
