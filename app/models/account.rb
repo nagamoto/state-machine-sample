64b93b981d9f6477a72c22679573c9bf946d57c7
ngmt <ngmt83ngmt@gmail.com>
2019-02-11 23:55:46 +0900

    Accountモデルに関連を実装

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,8 @@
 class Account < ApplicationRecord
+  has_one :activation, class_name: 'AccountActivation'
+  has_one :deactivation, class_name: 'AccountDeactivation'
+  has_one :ban, class_name: 'AccountBan'
+  has_one :active_suspension, lambda { where(removed_at: nil) }, class_name: 'AccountSuspension'
+
+  has_many :suspensions, class_name: 'AccountSuspension'
 end
